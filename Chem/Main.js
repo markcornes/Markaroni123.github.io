18ab9a2621a1650041e9b1ff80d12a31fab7b3be
Markaroni123 <cornes.mark@gmail.com>
2020-08-16 21:53:14 +1200

    chem start

---
+++ Chem/Main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("maincanvas");
+const ctx = canvas.getContext("2d");
+const runtext = document.getElementById("runtext");
+
+canvas.width = screen.width-200;
+canvas.height = screen.height-200;
